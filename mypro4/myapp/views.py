ac40d0b116a00dcb3eeb2210609c029296e9c5b9
Rukminisk <rukimini499@gmail.com>
2020-02-08 11:54:06 +0530

    First Project

---
+++ mypro4/myapp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def Test1(request):
+    return HttpResponse('<h1> It is a bright morning</h1>')
---
+++ mypro4/myapp4/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('hello1/', views.Test1),
+
+]
